b1506f1d3ab0c98e6d0cf86ac7f619a3c4695de6
unknown <Chamon562@gmail.com>
2021-11-16 17:46:40 -0500

    [users.js] Add name and email to lower case in case user has issues logging in and doesnt have to worry about it being case sensitive.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 
 const db = require("../../models");
+const { registerValidation } = require("../../validation");
 // const User = require("../models/User");
 // Postman http://localhost:8000/api/users/test
 router.get("/test", (req, res) => {
@@ -12,29 +13,33 @@ router.get("/test", (req, res) => {
 // REGISTER a USER
 // Postman route http://localhost:8000/api/users/register
 router.post("/register", (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
   // find user by email
   db.User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
       return res.status(400).json("Email already exist");
     } else {
-        
-        // creating a new user
-        const newUser = new db.User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        });
-        // adding bcrypt to hash password
-        // generating salt
+      // creating a new user
+      const newUser = new db.User({
+        name: req.body.name.toLowerCase(),
+        email: req.body.email.toLowerCase(),
+        password: req.body.password,
+      });
+      // adding bcrypt to hash password
+      // generating salt
       bcrypt.genSalt(10, (error, salt) => {
         bcrypt.hash(newUser.password, salt, (error, hash) => {
           if (error) throw error;
           // change the password to hash
           newUser.password = hash;
+          
           newUser
             .save()
             .then((createdUser) => res.json(createdUser))
-            .catch((err) => res.json(err));
+            .catch((error) => res.json(error));
         });
       });
     }
@@ -42,15 +47,15 @@ router.post("/register", (req, res) => {
 });
 
 //Postman route http://localhost:8000/api/users/6193f7c1975bdb73c252666f
-router.delete("/:id", (req,res) =>{
-    db.User.findByIdAndDelete(req.params.id)
-    .then(() =>{
-        res.status(204).json({message:"User has been deleted"})
-        console.log("User deleted")
+router.delete("/:id", (req, res) => {
+  db.User.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).json({ message: "User has been deleted" });
+      console.log("User deleted");
     })
-    .catch((err, ) =>{
-        res.json(err)
-    })
-})
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
