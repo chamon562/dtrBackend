2e996b58537e2da0d3606475f0b7408485396105
unknown <Chamon562@gmail.com>
2021-11-16 13:27:33 -0500

    [users.js] Add basic register route for user, todo: hash password

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-
+const User = require("../models/User");
 // Postman http://localhost:8000/api/users/test
 router.get("/test", (req, res) => {
-  res.json("users test route is working 😸");
+  res.status(200).json("users test route is working 😸");
+});
+
+
+router.post("/register", (req, res) => {
+    // find user by email
+    User.findOne({ email: req.body.email })
+    .then((user) => {
+        if (user) {
+            return res.status(400).json("Email already exist");
+        } else {
+            // creating a new user
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            });
+            newUser.save()
+            .then(createdUser => res.json(createdUser))
+            .catch(err=> res.json(err))
+        }
+    }).catch((err) =>{
+        res.status(400).json("Error trying to create a new user", err)
+    });
 });
 
 module.exports = router;
