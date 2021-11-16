0069c45e0129d589ce44ef785649ea8b45c41f87
unknown <Chamon562@gmail.com>
2021-11-16 14:36:50 -0500

    [validation.js] Add registerValidation function passing in data as a parameter and setting name email and password attribute for validation.

---
+++ validation.js
@@ -0,0 +1,14 @@
+// Validation
+const Joi = require("@hapi/joi");
+
+// Register validation
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
