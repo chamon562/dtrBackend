409ce40ef329a61f746d729969f39efb3da13f39
unknown <Chamon562@gmail.com>
2021-11-16 13:03:54 -0500

    [users.js] Forgot to add module.exports

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 
-router.get("/test", (req, res) =>{
-    res.json("users test route is working 😸")
-})
+router.get("/test", (req, res) => {
+  res.json("users test route is working 😸");
+});
+
+module.exports = router;
