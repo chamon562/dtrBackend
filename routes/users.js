90b4c18b6d75ec8d9f5241631ee42eee779ddb65
unknown <Chamon562@gmail.com>
2021-11-16 13:02:15 -0500

    [users.js] Add basic test get route

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+router.get("/test", (req, res) =>{
+    res.json("users test route is working 😸")
+})
